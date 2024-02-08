520772175c145942a29d787476bec4984921a91e
caiman48 <caiman48clm@gmail.com>
2024-02-08 09:53:54 -0500

    starting with the Javascript

---
+++ Assets/Script/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var startButton = document.getElementById('start-btn');
+    var questionContainerElement = document.getElementById('question-container');
+    var questionElement = document.getElementById('question');
+    var answerButtonsElement = document.getElementById('answer-buttons');
+    var timerDisplay = document.getElementById('timer');
+    var endScreen = document.getElementById('end-screen');
+    var finalScoreElement = document.getElementById('final-score');
+    var initialsInputEl = document.getElementById('initials');
+    var saveScoreButton = document.getElementById('save-score');
+    
