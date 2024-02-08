8adb4c69e73e31e5bdf43ba7e83ebf0175f228ca
caiman48 <caiman48clm@gmail.com>
2024-02-08 09:57:53 -0500

    added first question git add -Agit add -A!

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -8,4 +8,22 @@ document.addEventListener('DOMContentLoaded', () => {
     var finalScoreElement = document.getElementById('final-score');
     var initialsInputEl = document.getElementById('initials');
     var saveScoreButton = document.getElementById('save-score');
-    
+
+    let currentQuestionIndex, timerCountdown;
+    let score = 0;
+
+    const questions = [
+        {
+        question: "what does 'var' stand for?",
+        answers: [
+            { text: 'variable', correct: true },
+            { text: 'value', correct: false },
+            { text: 'victory', correct: false },
+            { text: 'variance', correct: false }
+        ]
+        },
+        {
+
+
+
+        }
